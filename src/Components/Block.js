14c4141d3f9898908c32fdac9d493f3e47d66027
Alexandr Zolotar <zolotar333@gmail.com>
2021-07-18 18:51:56 +0300

    api vk test

--- src/Components/Block.js
+++ src/Components/Block.js
@@ -6,17 +6,63 @@ const css = {
     }
 }
 
-function click(){
-    let paramsString = document.location.href,
-        searchParams = new URLSearchParams(paramsString);
-    console.log(searchParams.get("http://localhost/#access_token"));
-    let token = searchParams.get("http://localhost/#access_token");
+var a;
+var b = false;
+
+
+
+
+// function ass(){
+//     console.log('jepa');
     
-    if (token === null){
-        alert("Вы не авторизованы");
-    }else{
-        alert("Вы авторизованы");
-    }
+// }
+
+function hashHandler(){
+    var oldHash = a.location.hash;
+    // var url = (a.location != a.parent.location)
+    //         ? document.referrer
+    //         : document.location.href;
+    var detect = () => {
+        if(oldHash!=a.location.hash){
+            console.log(a.location.hash);
+            oldHash = a.location.hash;
+        }
+        // a.onhashchange = function() { 
+        //         console.log("piska")
+        //     }
+    };
+    setInterval(function(){ detect() }, 100);
+}
+
+// var hashDetection = new hashHandler();
+
+
+
+function click(){
+    b = true;
+    // document.location.href = "https://oauth.vk.com/authorize?client_id=7905028&display=page&redirect_uri=localhost:80&scope=groups&response_type=token&v=5.52";
+    // let paramsString = document.location.href,
+    //     searchParams = new URLSearchParams(paramsString);
+    // console.log(searchParams.get("http://localhost/#access_token"));
+    // let token = searchParams.get("http://localhost/#access_token");
+    
+    // if (token === null){
+    //     alert("Вы не авторизованы");
+    // }else{
+    //     alert("Вы авторизованы");
+    // }
+    a = window.open("https://oauth.vk.com/authorize?client_id=7905028&display=page&redirect_uri=localhost:80&scope=groups&response_type=token&v=5.52","","height=300,width=400");
+
+    // console.log(a.location.href);
+    // a.onhashchange = function() { 
+    //     console.log("piska")
+    //     console.log('a');
+    // }
+    hashHandler();
+}
+
+function tested(){
+    b = true;
 }
 
 function Block() {
@@ -25,6 +71,7 @@ function Block() {
      <h1>Test</h1>
         <form action=''>
             <input type='button' onClick={click}  value='Тык'></input>
+            <input type='button' onClick={tested}  value='Бяк'></input>
             {/* <a href='https://oauth.vk.com/authorize?client_id=7905028&display=page&redirect_uri=localhost:80&scope=groups&response_type=token&v=5.52'>123</a> */}
         </form>
     </div>
