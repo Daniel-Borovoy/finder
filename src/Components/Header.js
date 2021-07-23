866b553655d312c88a85695b9a353d6dd6e491d1
Daniel <74528634+Daniel-Borovoy@users.noreply.github.com>
2021-07-23 20:11:31 +0300

    fix navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,20 +4,22 @@ import VkLogin from  '../Actions/VkLogin'
 
 function Header() {
   return (
-    <header className="header">
-      <main>  
-        <h1 className="text">FINDER</h1>
-        <nav>
-          <ul className="links">
-              <li><button>ссылка</button></li>
-              <li><button>ссылка</button></li>
-              <li><button>ссылка</button></li>
-              <li><button>ссылка</button></li>
-          </ul>
-            <VkLogin/>
-        </nav>
-      </main>
-    </header>
+      <div className="navbar">
+        <header className="header">
+          <main>
+            <h1 className="text">FINDER</h1>
+            <nav>
+              <ul className="links">
+                  <li><button>ссылка</button></li>
+                  <li><button>ссылка</button></li>
+                  <li><button>ссылка</button></li>
+                  <li><button>ссылка</button></li>
+              </ul>
+                <VkLogin/>
+            </nav>
+          </main>
+        </header>
+      </div>
   );
 }
 
