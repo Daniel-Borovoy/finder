8284378b4492440d15fec70f73f9013821583b4a
Daniel <74528634+Daniel-Borovoy@users.noreply.github.com>
2021-07-28 00:09:10 +0300

    top_menu

--- frontend/src/Actions/VkLogin.js
+++ frontend/src/Actions/VkLogin.js
@@ -12,6 +12,7 @@ export let userId;
 let login_button = 'Login in';
 // ссылка на аватар юзера
 let profile_src;
+let profile_name;
 // стиль для формы
 const form_style = {
   position: 'relative',
@@ -24,8 +25,9 @@ const profile_img_style = {
   marginLeft: '15px',
   width: '35px',
   borderRadius: '50%',
-  position: 'absolute',
-  left: '100%',
+  marginRight: '10px'
+  // position: 'absolute',
+  // left: '100%',
 };
 let btn_style = {
   backgroundColor: 'blue',
@@ -44,6 +46,8 @@ function connect(r) {
     VK.Api.call('users.get', { user_ids: r.session.mid, fields: 'photo_50', v: '5.73' }, (r) => {
       if (r.response) {
         profile_src = r.response[0].photo_50;
+        profile_name = r.response[0].first_name;
+        console.log(r.response[0])
       }
     });
     ReactDOM.render(<App />, document.getElementById('root'));
@@ -61,6 +65,12 @@ VK.Auth.getLoginStatus((r) => {
   }
 });
 
+const divStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  fontSize: '12px',
+}
 class VkLogin extends React.Component {
   
   click() {
@@ -85,12 +95,28 @@ class VkLogin extends React.Component {
   }
 
   render() {
-    return (
-      <div style={form_style} id='vk'>
-        <button type="button" style={btn_style} onClick={this.click}>{login_button}</button>
-        <img src={profile_src} style={profile_img_style} alt="" />
-      </div>
-    );
+    if (session) {
+      return (
+          <div style={divStyle} className="log">
+            <h3>{profile_name}</h3>
+            <img src={profile_src} style={profile_img_style} alt="" />
+            <svg fill="none" height="8" viewBox="0 0 12 8" width="12" xmlns="http://www.w3.org/2000/svg"><path clip-rule="evenodd" d="M2.16 2.3a.75.75 0 011.05-.14L6 4.3l2.8-2.15a.75.75 0 11.9 1.19l-3.24 2.5c-.27.2-.65.2-.92 0L2.3 3.35a.75.75 0 01-.13-1.05z" fill="currentColor" fill-rule="evenodd"></path></svg>
+            <div className="top_profile_menu">
+              <a>Настройки</a>
+              <a>Помощь</a>
+              <button onClick={this.click}>Выйти</button>
+            </div>
+          </div>
+      )
+    }
+    else{
+      return (
+          <div style={form_style} id='vk'>
+            <button type="button" style={btn_style} onClick={this.click}>{login_button}</button>
+            <img src={profile_src} style={profile_img_style} alt="" />
+          </div>
+      )
+    }
   }
 }
 
