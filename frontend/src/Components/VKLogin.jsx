2a92877a2b18bd040fe132cdad3395de215ff05a
Daniel <74528634+Daniel-Borovoy@users.noreply.github.com>
2021-08-07 19:06:45 +0300

    fix warnings

--- frontend/src/Components/VKLogin.jsx
+++ frontend/src/Components/VKLogin.jsx
@@ -37,15 +37,6 @@ const profileStyle = {
   fontSize: '12px',
 }
 
-
-
-
-
-
-  
-
-
-
 class VKLogin extends React.Component {
 
   constructor(props) {
@@ -135,7 +126,7 @@ class VKLogin extends React.Component {
           <div style={profileStyle} className={this.state.open ? "profile open" : "profile"} onClick={this.setOpen}>
             <h3>{profileName}</h3>
             <img src={profileSRC} style={profileImgStyle} alt="" />
-            <svg className={this.state.open ? "arrow rotate" : "arrow"} fill="none" height="8" viewBox="0 0 12 8" width="12" xmlns="http://www.w3.org/2000/svg"><path clip-rule="evenodd" d="M2.16 2.3a.75.75 0 011.05-.14L6 4.3l2.8-2.15a.75.75 0 11.9 1.19l-3.24 2.5c-.27.2-.65.2-.92 0L2.3 3.35a.75.75 0 01-.13-1.05z" fill="currentColor" fill-rule="evenodd"></path></svg>
+            <svg className={this.state.open ? "arrow rotate" : "arrow"} fill="none" height="8" viewBox="0 0 12 8" width="12" xmlns="http://www.w3.org/2000/svg"><path clipRule="evenodd" d="M2.16 2.3a.75.75 0 011.05-.14L6 4.3l2.8-2.15a.75.75 0 11.9 1.19l-3.24 2.5c-.27.2-.65.2-.92 0L2.3 3.35a.75.75 0 01-.13-1.05z" fill="currentColor" fillRule="evenodd"></path></svg>
             <div className={this.state.open ? "top_profile_menu open" : "top_profile_menu"} >
               <div className="top_profile_sep"></div>
               <button>Настройки</button>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import App from './App.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root'),
 );
 
